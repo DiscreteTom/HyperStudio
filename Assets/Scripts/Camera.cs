a3fbbb9bb8b058232b017ea400848c0d5d2953d5
DiscreteTom <discrete_tom@outlook.com>
2023-03-03 00:03:09 +0800

    add camera script

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class App : MonoBehaviour {
+  [DllImport(@"Assets/XR/XRSDK.dll")]
+  public static extern void XRSDK_Init();
+  [DllImport(@"Assets/XR/XRSDK.dll")]
+  public static extern long GetArSensor();
+
+  // Start is called before the first frame update
+  void Start() {
+    XRSDK_Init();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    float pitch, yaw, roll; // range: [-1, 1]
+    unsafe {
+      long addr = GetArSensor();
+      pitch = *(float*)(addr + 44);
+      yaw = *(float*)(addr + 48);
+      roll = *(float*)(addr + 52);
+    }
+
+    this.transform.eulerAngles = new Vector3(pitch, yaw, roll) * 180;
+  }
+}
