32dee328b83adf7ba26992390d1f4027b32404e8
DiscreteTom <discrete_tom@outlook.com>
2023-03-03 21:02:19 +0800

    hold tab to show help text

---
+++ Assets/Scripts/HelpText.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using DT.General;
+using TMPro;
+using UnityEngine;
+
+public class HelpText : CBC {
+  void Start() {
+    var text = this.GetComponent<TMP_Text>();
+    text.text = @"Hold `Tab` to show this help.
+Drag screen using mouse to move the screen.
+Scroll to scale the screen.
+Scroll while pressing `Ctrl` to push away / pull close a screen.
+Press `ESC` to remove a screen.
+Hold `Ctrl + '+'/'-'` to bend the screen.
+Press `Ctrl + 0` to toggle bend.
+Hold `Ctrl + J/K/L/U/I/O` to rotate screen.
+Press `Ctrl + R` to reset viewpoint direction.
+Press `Ctrl + Shift + R` to reload the scene.
+Press `Ctrl + S` to save screen's location, rotation and scale to config.
+Hold `Ctrl + V` to record view zone for the screen.
+Press `Ctrl + Shift + V` to disable view zone for the screen.
+Hold `Ctrl + A` to show all screens(not include removed).
+Press `Ctrl + F` to toggle `AutoLookAtCamera`.
+Press `Alt + F4` to exit.";
+
+    this.OnUpdate.AddListener(() => {
+      if (Input.GetKey(KeyCode.Tab)) text.enabled = true;
+      else text.enabled = false;
+    });
+  }
+}
--- README.md
+++ README.md
@@ -19,6 +19,7 @@ HyperStudio.exe -monitor N
 
 ## How to Use
 
+- Hold `Tab` to show this help.
 - Drag screen using mouse to move the screen.
 - Scroll to scale the screen.
 - Scroll while pressing `Ctrl` to push away / pull close a screen.
