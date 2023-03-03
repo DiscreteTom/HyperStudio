65db5281561ec454481180933f251587f58b86a6
DiscreteTom <discrete_tom@outlook.com>
2023-03-03 12:16:34 +0800

    optimize monitor position & scale

--- Assets/Scripts/MonitorManager.cs
+++ Assets/Scripts/MonitorManager.cs
@@ -4,12 +4,23 @@ public class MonitorManager : MonoBehaviour {
   [SerializeField] GameObject monitorPrefab;
 
   void Start() {
+    // center of primary monitor
+    var primaryCenter = this.GetCenter(uDesktopDuplication.Manager.primary);
+
     for (var i = 0; i < uDesktopDuplication.Manager.monitors.Count; i++) {
       var monitor = uDesktopDuplication.Manager.monitors[i];
       var obj = Instantiate(this.monitorPrefab, this.transform);
       var texture = obj.GetComponent<uDesktopDuplication.Texture>();
       texture.monitorId = i;
-      obj.transform.position = new Vector3(i * 2, 0, 0);
+      obj.transform.localScale = new Vector3(monitor.width, monitor.height, 1000) / 1000;
+      // set screen position just like in the system settings
+      obj.transform.position = (this.GetCenter(monitor) - primaryCenter) / 100;
+      // look at camera
+      obj.transform.LookAt(2 * obj.transform.position - Camera.main.transform.position);
     }
+  }
+
+  Vector3 GetCenter(uDesktopDuplication.Monitor monitor) {
+    return new Vector3(monitor.left + (monitor.right - monitor.left) / 2, -(monitor.top + (monitor.bottom - monitor.top) / 2), 0);
   }
 }
