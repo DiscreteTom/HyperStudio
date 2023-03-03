476a4f823c5b9115a623798afa608791dec92c8c
DiscreteTom <discrete_tom@outlook.com>
2023-03-03 16:17:41 +0800

    fix comments

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -5,7 +5,7 @@ public class App : MonoBehaviour {
   void Start() {
     var handle = Win32.GetActiveWindow();
     // -20: GWL_EXSTYLE
-    // 0x80: WS_EX_TRANSPARENT
+    // 0x80: WS_EX_TOOLWINDOW, show window on all desktops
     Win32.SetWindowLongA(handle, -20, 0x80);
   }
 #endif
