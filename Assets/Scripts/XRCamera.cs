8a1f0ff5a5f9d66ca7eb6183c9b528a1c52daa8c
DiscreteTom <discrete_tom@outlook.com>
2023-03-03 11:36:07 +0800

    move XRSDK to a single file

--- Assets/Scripts/XRCamera.cs
+++ Assets/Scripts/XRCamera.cs
@@ -1,32 +1,15 @@
-using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class XRCamera : MonoBehaviour {
-#if UNITY_EDITOR
-  [DllImport(@"Assets/XR/XRSDK.dll")]
-  public static extern void XRSDK_Init();
-  [DllImport(@"Assets/XR/XRSDK.dll")]
-  public static extern void Reset();
-  [DllImport(@"Assets/XR/XRSDK.dll")]
-  public static extern long GetArSensor();
-#else
-  [DllImport(@"HyperStudio_Data/Plugins/x86_64/XRSDK.dll")]
-  public static extern void XRSDK_Init();
-  [DllImport(@"HyperStudio_Data/Plugins/x86_64/XRSDK.dll")]
-  public static extern void Reset();
-  [DllImport(@"HyperStudio_Data/Plugins/x86_64/XRSDK.dll")]
-  public static extern long GetArSensor();
-#endif
-
   void Start() {
-    XRSDK_Init();
-    Reset();
+    XRSDK.XRSDK_Init();
+    XRSDK.Reset();
   }
 
   void Update() {
     float x, y, z, w; // range: [-1, 1]
     unsafe {
-      long addr = GetArSensor();
+      long addr = XRSDK.GetArSensor();
       x = *(float*)(addr + 44);
       y = *(float*)(addr + 48);
       z = *(float*)(addr + 52);
