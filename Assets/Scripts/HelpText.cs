454290eeede72f0f59201792b77f92ca973ebb41
DiscreteTom <discrete_tom@outlook.com>
2023-06-06 20:49:14 +0800

    chore: update HelpText

--- Assets/Scripts/HelpText.cs
+++ Assets/Scripts/HelpText.cs
@@ -12,14 +12,10 @@ Drag screen using mouse to move the screen.
 Scroll to scale the screen.
 Scroll while pressing `Ctrl` to push away / pull close a screen.
 Press `Backspace` to remove a screen.
-Hold `Ctrl + '+'/'-'` to bend the screen.
-Press `Ctrl + 0` to toggle bend.
 Hold `Ctrl + J/K/L/U/I/O` to rotate screen.
 Press `Ctrl + R` to reset viewpoint direction.
 Press `Ctrl + Shift + R` to reload the scene.
 Press `Ctrl + S` to save screen's location, rotation and scale to config.
-Hold `Ctrl + V` to record view zone for the screen.
-Press `Ctrl + Shift + V` to disable view zone for the screen.
 Hold `Ctrl + A` to show all screens(not include removed).
 Press `Ctrl + F` to toggle `AutoLookAtCamera`.
 Press `ESC` to exit.";
